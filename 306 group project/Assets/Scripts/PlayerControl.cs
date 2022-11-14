be15ae0f2413e5193e590e020b035eab0a10019e
RayMa <88625717+RayWangyangMa@users.noreply.github.com>
2022-11-13 19:06:52 -0600

    changed from 3d to 2d

--- 306 group project/Assets/Scripts/PlayerControl.cs
+++ 306 group project/Assets/Scripts/PlayerControl.cs
@@ -54,7 +54,7 @@ public class PlayerControl : MonoBehaviour
     }
 
 
-    private void OnCollision2DEnter(Collision collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
 
         if (collision.gameObject.tag=="ground")
