466580acb3f36e068ba142dd36f287f04dd8fb7a
RayMa <88625717+RayWangyangMa@users.noreply.github.com>
2022-11-13 19:13:30 -0600

    fixed the double jump bug

--- 306 group project/Assets/Scripts/PlayerControl.cs
+++ 306 group project/Assets/Scripts/PlayerControl.cs
@@ -57,7 +57,7 @@ public class PlayerControl : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
 
-        if (collision.gameObject.tag=="ground")
+        if (collision.gameObject.tag=="Ground" || collision.gameObject.tag=="ground")
         {
             jumpInt = 2;
         }
