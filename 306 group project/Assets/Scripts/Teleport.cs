89fcf1eb2756619d16d50c3c9a08736ce965963d
Kim <ykm535@usask.ca>
2022-11-20 23:33:47 -0600

    made the portals better, entry portal transports player to the nearest exit portal

--- 306 group project/Assets/Scripts/Teleport.cs
+++ 306 group project/Assets/Scripts/Teleport.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class Teleport : MonoBehaviour
 {
-    [SerializeField] public GameObject Portal;
-    [SerializeField] public GameObject Player;
+    private GameObject Portal;
+    private GameObject Player;
+    public GameObject[] portalsFound;
     [SerializeField] private AudioSource portalSound;
+
+    private float oldDistance = 9999;
+
+    void Start() {
+        portalsFound = GameObject.FindGameObjectsWithTag("PortalEnd");
+        foreach (GameObject portalEnd in portalsFound)
+        {
+            float dist = Vector2.Distance(this.gameObject.transform.position, portalEnd.transform.position);
+            if (dist < oldDistance)
+            {
+                Portal = portalEnd;
+                oldDistance = dist;
+            }
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player") {
+            Player = other.gameObject;
             StartCoroutine (teleportation());
             portalSound.Play();
         }
