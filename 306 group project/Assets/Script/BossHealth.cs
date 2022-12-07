c2d821f56d63ed18aff402b32a0d662f0644eb2c
chikaluv <ducthang19112001@gmail.com>
2022-12-06 21:34:40 -0600

    commit

--- 306 group project/Assets/Script/BossHealth.cs
+++ 306 group project/Assets/Script/BossHealth.cs
@@ -23,7 +23,7 @@ public class BossHealth : MonoBehaviour
             GetComponent<Animator>().Play("Die");
             Destroy(gameObject);
             //Instantiate((GameObject)Resources.Load("Dragon_Egg_01"), new Vector3(205, 203), Quaternion.identity);
-           // GameObject.FindGameObjectWithTag("portal_end_2").SetActive(true);
+            //GameObject.FindGameObjectWithTag("portal_end_2").SetActive(true);
         }
     }
 
